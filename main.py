fbac6fe48519515f6c3576699ae25a1f55f11f7f
Anna Kolodziejcyk <akk962@tamu.edu>
2020-01-26 10:09:54 -0600

    Update main.py
    Setting port

--- main.py
+++ main.py
@@ -148,7 +148,8 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    port = os.getenv("PORT", 8080)
+    app.run(host='127.0.0.1', port=port, debug=True)
 # [END gae_python37_app]
 
 '''
