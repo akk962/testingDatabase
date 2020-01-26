eecd46280514e82a34111b4bf68ed2707f73ca1a
Anna Kolodziejcyk <akk962@tamu.edu>
2020-01-26 10:29:37 -0600

    update host

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     port = os.getenv("PORT", 8080)
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
 # [END gae_python37_app]
 
 '''
